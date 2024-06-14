f0597d504491ce80cb7378e3661f7bbf91449081
Jose Perdomo <jose.gabriel92@hotmail.com>
2024-06-14 12:31:23 -0400

    Creacion de componentes MealList

--- src/components/MealList/MealList.jsx
+++ src/components/MealList/MealList.jsx
@@ -1,11 +1,30 @@
+import { useEffect, useState } from "react";
+import React from "react";
+import "./MealList.css";
 
+export default function MealList() {
+  const [data, setData] = useState([]);
+  const [contador, setContador] = useState(1);
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood"
+      );
+      const jsonResponse = await response.json();
+      setData(jsonResponse.meals);
+      console.log(jsonResponse.meals);
+    }
+    getData(); // Llama a la función cuando el componente se monte
+  }, []);
 
-function MealList() {
   return (
-    <div>
-      MealList
+    <div className="meal-list">
+      {data.map((item) => (
+        <div className="meal-contenedor">
+          <img src={item.strMealThumb} alt="" />
+          <p>{item.strMeal}</p>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
-
-export default MealList
